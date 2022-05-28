dc82520ed96b76fb0e67a8a9b49aa748fc06e065
Vitor Barreto <vitor.miguel0581@gmail.com>
2022-05-28 12:13:17 -0300

    !!

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+import Image from 'next/image';
+
+
+export function Header() {
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width="110" height="31"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Inicio | ig.news</title>
+    </Head>
+
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
