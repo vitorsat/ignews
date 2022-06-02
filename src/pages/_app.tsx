4df3f99525cd24513976f3887410ae0f19917c4e
Vitor Barreto <vitor.miguel0581@gmail.com>
2022-06-02 16:34:57 -0300

    feat: add github auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from 'next/app'
 
 import '../../styles/global.scss'
 import { Header } from '../components/Header'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-  <>
+  <SessionProvider session={session}>
   <Header />
   <Component {...pageProps} />
-  </>
+  </SessionProvider>
   )
 }
 
